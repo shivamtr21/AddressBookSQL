227c74ec6fbcca957c5603fb619eb622527c93ed
shivamtr21 <shivamtripathi26297@gmail.com>
2023-07-14 08:20:32 +0530

    [ shivam tripathi ] :Added Addresss Book Sql UC5

--- AddressBook.sql
+++ AddressBook.sql
@@ -35,3 +35,11 @@ WHERE first_name = 'Aakanksha' AND last_name = 'Pandey';
 
 DELETE FROM address_book WHERE first_name = 'Shivam'
 
+INSERT INTO address_book VALUES('Ritu','Pandey','Sector-2','Bhilai','CG','456786','90456723','ritu@gmail.com'),
+                               ('Shivay','Sharma','Sector-c','Bhilai','CG','456445','984839433','shivay@.com')
+
+
+SELECT * FROM address_book WHERE city = 'Bhilai'      /* search by city */
+SELECT * FROM address_book WHERE state = 'MP'        /* search by state */
+SELECT DISTINCT city FROM address_book              /* distinct values */
+SELECT DISTINCT state FROM address_book
