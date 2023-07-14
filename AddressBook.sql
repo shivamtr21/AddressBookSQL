a10a2698eab890bb7e6aa9d43ac1a085dd07854f
shivamtr21 <shivamtripathi26297@gmail.com>
2023-07-14 08:08:27 +0530

    [ shivam tripathi ] :Added Addresss Book Sql UC4

--- AddressBook.sql
+++ AddressBook.sql
@@ -22,3 +22,13 @@ SELECT * FROM address_book
 INSERT INTO address_book VALUES('Aakanksha','Pandey','Sector-7','Bhilai','CG','490006','908765323','aakan@gmail.com'),
 								('Divya','Mishra','Block2','Lucknow','UP','490566','90845723','divya@gmail.com'),
 								('Shivam','Tripathi','Area -8/11','Bhopal','MP','370006','903456323','shivam@gmail.com')
+
+
+
+								UPDATE address_book SET address = '456 New Lucknow', zip = '67890', 
+phone_number = '5555678', email = 'divya612@gmail.com'
+WHERE first_name = 'Divya' AND last_name = 'Mishra'
+
+UPDATE address_book SET address = '456 New Bihar', zip = '78990', 
+phone_number = '987537', email = 'aakansha12@gmail.com'
+WHERE first_name = 'Aakanksha' AND last_name = 'Pandey';
