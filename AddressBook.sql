30676bd55a4da7a883572b53cae8b6345b62876c
shivamtr21 <shivamtripathi26297@gmail.com>
2023-07-14 07:59:28 +0530

    [ shivam tripathi ] :Added Addresss Book Sql UC2

--- AddressBook.sql
+++ AddressBook.sql
@@ -3,3 +3,18 @@
    CREATE DATABASE  address_book_service
 
    USE  address_book_service
+
+
+    CREATE TABLE address_book (
+  id INT PRIMARY KEY IDENTITY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  address VARCHAR(100) NOT NULL,
+  city VARCHAR(50) NOT NULL,
+  state VARCHAR(50) NOT NULL,
+  zip VARCHAR(20) NOT NULL,
+  phone_number VARCHAR(20) NOT NULL,
+  email VARCHAR(100) NOT NULL
+)
+
+SELECT * FROM address_book
